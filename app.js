cd330493b4ed8aa3732f017ba5fb94e06d9482df
ajaythopate <ajaythopate999@gmail.com>
2022-09-15 21:31:42 +0530

    first commit

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-var btnTranslet = document.querySelector("#btn-translet");
-var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#output");
+function translator() {
+    var inputvalue = document.getElementById("123").value;
+    var op = document.getElementById("456");
+    console.log(inputvalue);
 
-console.log(txtInput);
-function clickhandler() {
-    outputDiv.innerText = "ajsjsjsjjsjsj" + txtInput.value
-};
-btnTranslet.addEventListener("click", clickhandler)
+    var str1 = "https://api.funtranslations.com/translate/minion.json";
+    var str2 = "?";
+    var str3 = "text=";
+    var texttobtanslated = str1 + str2 + str3 + inputvalue;
+    fetch(texttobtanslated)
+        .then((response) => response.json())
+        .then((json) => {
+            console.log(json.contents.translated);
+            var output = json.contents.translated;
+            op.innerText = output;
+        })
+        .catch(error => {
+            console.log(error);
+            op.innerText = "We are unable to keep up with your pace. Please try again in an hour.";
+            op.classList.add("error");
+        });
+
+}
